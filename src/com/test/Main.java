8f764a91827175aaf89dd6c0cc36b865a8493f25
takuhiro55 <takuhiro55@gmail.com>
2020-09-03 21:33:35 -0700

    new feature was added

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -3,7 +3,10 @@ package com.test;
 public class Main {
 
     public static void main(String[] args) {
+        Calc_Interface calc = new Cashier(3.5, 5.5);
         System.out.println("Hello World.");
+        System.out.println(calc.sum());
+        System.out.println(calc.sum());
     }
 
 }
